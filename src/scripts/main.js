2bdd3de5a4c6e2c4116b002bed5fdd097f0a205f
Mykola Gataylo <mykola.gataylo@gmail.com>
2020-12-04 18:26:36 +0100

    section 'contacts' with adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
-class Test {
-  constructor() {
-    console.log(1 + 1 + 1);
-  }
-}
+  function initMap() {
+    const mapOptions = {
+      center: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.roadmap,
+      scrollwheel: false,
+      draggable: false,
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: true,
+      streetViewControl: true,
+      overviewMapControl: true,
+      rotateControl: true,
+    };
 
-const test = new Test();
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    new google.maps.Marker({
+      position: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
+      map,
+      animation: google.maps.Animation.BOUNCE,
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initMap);
