ef738af625ce7253e0f45ce85712b2f78f85f814
Mykola Gataylo <mykola.gataylo@gmail.com>
2020-12-06 20:08:30 +0100

    added svg p.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,54 @@
-  function initMap() {
-    const mapOptions = {
-      center: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.roadmap,
-      scrollwheel: false,
-      draggable: false,
-      panControl: false,
-      zoomControl: false,
-      mapTypeControl: false,
-      scaleControl: true,
-      streetViewControl: true,
-      overviewMapControl: true,
-      rotateControl: true,
-    };
+    // GoogleMaps
 
-    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    function initMap() {
+      const mapOptions = {
+        center: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.roadmap,
+        scrollwheel: false,
+        draggable: false,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        rotateControl: true,
+      };
 
-    new google.maps.Marker({
-      position: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
-      map,
-      animation: google.maps.Animation.BOUNCE,
-    });
-  }
+      const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  google.maps.event.addDomListener(window, 'load', initMap);
-
-  // class active to navbar-link
-
-  let NavLink = document.getElementsByClassName("nav-link-custom");
-
-  for (let i = 0; i < NavLink.length; i++) {
-    NavLink[i].addEventListener('click', function () {
-      for (let i = 0; i < NavLink.length; i++) {
-        NavLink[i].classList.remove('active');
-      }
-      this.classList.add('active');
-    })
-  }
-
-  let scroll = scrollY;
-  const meetBlock = document.getElementById("meet");
-
-  window.addEventListener("scroll", function () {
-    if (scrollY > scroll) {
-      meetBlock.classList.add("animate__flip");
-    } else {
-      meetBlock.classList.remove("animate__flip");
+      new google.maps.Marker({
+        position: new google.maps.LatLng(36.69975876585313, -4.4466650226033595),
+        map,
+        animation: google.maps.Animation.BOUNCE,
+      });
     }
-  });
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+    // class active to navbar-link
+
+    let NavLink = document.getElementsByClassName("nav-link-custom");
+
+    for (let i = 0; i < NavLink.length; i++) {
+      NavLink[i].addEventListener('click', function () {
+        for (let i = 0; i < NavLink.length; i++) {
+          NavLink[i].classList.remove('active');
+        }
+        this.classList.add('active');
+      })
+    }
+
+    // animation
+
+    let scroll = scrollY;
+    const meetBlock = document.getElementById("meet");
+
+    window.addEventListener("scroll", function () {
+      if (scrollY > scroll) {
+        meetBlock.classList.add("animate__flip");
+      } else {
+        meetBlock.classList.remove("animate__flip");
+      }
+    });
